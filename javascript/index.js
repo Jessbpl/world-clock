58bb905052df7e439bdcaa443c0d6ff0080bebd7
Jessbpl <jessbrapl@gmail.com>
2023-12-07 13:43:50 +0100

    added current location code & final touches

--- javascript/index.js
+++ javascript/index.js
@@ -7,7 +7,7 @@ function updateTime() {
     let chicagoTimeElement = chicagoElement.querySelector(".time");
     let chicagoTime = moment().tz("America/Chicago");
 
-    chicagoDateElement.innerHTML = chicagoTime.format("ddd MMMM Do, YYYY");
+    chicagoDateElement.innerHTML = chicagoTime.format("ddd, MMMM Do, YYYY");
     chicagoTimeElement.innerHTML = chicagoTime.format(
       "h:mm:ss[<small>]A[</small>]"
     );
@@ -19,7 +19,7 @@ function updateTime() {
     let stockholmTimeElement = stockholmElement.querySelector(".time");
     let stockholmTime = moment().tz("Europe/Stockholm");
 
-    stockholmDateElement.innerHTML = stockholmTime.format("ddd MMMM Do, YYYY");
+    stockholmDateElement.innerHTML = stockholmTime.format("ddd, MMMM Do, YYYY");
     stockholmTimeElement.innerHTML = stockholmTime.format(
       "h:mm:ss[<small>]A[</small>]"
     );
@@ -28,21 +28,25 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
-  console.log(cityTime.format("ddd MMMM Do, YYYY"));
+  console.log(cityTime.format("ddd, MMMM Do, YYYY"));
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
   <div class="city">
           <div>
             <h2>${cityName}</h2>
-            <div class="date">${cityTime.format("ddd MMMM Do, YYYY")}</div>
+            <div class="date">${cityTime.format("ddd, MMMM Do, YYYY")}</div>
           </div>
           <div class="time">${cityTime.format(
             "h:mm:ss"
           )}<small>${cityTime.format("A")}</small></div>
         
-      </div>`;
+      </div>
+      <a href="index.html" class="reset">Reset</a>`;
 }
 
 updateTime();
